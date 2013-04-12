2e2c2ead0c7b2005911626af75420a5e48255af6
cmsbuild <>
2013-04-12 16:16:25 +0000

    This commit was manufactured by cvs2git to create tag 'TauAnalysis- CandidateTools-b5_3_x_2013Apr12'.
    Sprout from master 2013-04-12 16:16:24 UTC Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch> 'added protection against unphysical large e/mu masses'
    Cherrypick from master 2013-01-17 16:22:36 UTC Roger Wolf <sha1-bf6c89fe049b9f9a193bed584ffb80166e4ef256@cern.ch> 'port from NSVfitStandalone branch':
        interface/NSVfitStandaloneAlgorithm.h
        plugins/SVfitTrackInfoAnalyzer.h

--- interface/NSVfitStandaloneAlgorithm.h
+++ interface/NSVfitStandaloneAlgorithm.h
@@ -90,13 +90,9 @@ namespace NSVfitStandalone{
     ~MCPtEtaPhiMassAdapter()
     {
       delete histogramPt_;
-      delete histogramPt_density_;
       delete histogramEta_;
-      delete histogramEta_density_;
       delete histogramPhi_;
-      delete histogramPhi_density_;
       delete histogramMass_;
-      delete histogramMass_density_;
     }
     void SetNDim(int nDim) { nDim_ = nDim; }
     unsigned int NDim() const { return nDim_; }
